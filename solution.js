227b78cbd280523e20b62a25a23ea34c4aa57f34
chris <chrishethertonweb@gmail.com>
2022-07-06 14:00:30 +0200

    03 done

--- solution.js
+++ solution.js
@@ -10,3 +10,7 @@ console.log(
 let specificNum = 10;
 // console.log(specificNum < 19 ? specificNum - 19 : (19 - specificNum) * 3);
 console.log(specificNum < 19 ? 19 - specificNum : (specificNum - 19) * 3);
+
+console.log(
+  numOne === 50 || numTwo === 50 || numOne + numTwo === 50 ? true : false
+);
